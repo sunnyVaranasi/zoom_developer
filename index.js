0c51d8f0fffe8081dfd7161c0b93d089b0222432
sunnyVaranasi <121848911+sunnyVaranasi@users.noreply.github.com>
2023-01-06 13:32:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ if(cluster.isMaster){
     }
 
     cluster.on('online', function(worker){
-        console.log('Product Worker ' + worker.process.pid + ' is online');
+        console.log('Dev Worker ' + worker.process.pid + ' is online');
     });
     cluster.on('exit', function(worker, code, signal){
         console.log('Worker ' + worker.process.pid + ' died');
